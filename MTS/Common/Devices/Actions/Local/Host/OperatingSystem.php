1881182017afcbf656b0afde86b483a905dec711
Martin Madsen <github@martinpetermadsen.com>
2017-01-04 14:24:49 +0000

    Standardized how distribution is determined for Linux
    Both local and remote classes.

--- MTS/Common/Devices/Actions/Local/Host/OperatingSystem.php
+++ MTS/Common/Devices/Actions/Local/Host/OperatingSystem.php
@@ -21,18 +21,15 @@ class OperatingSystem extends Base
 			//it also does not change without a reboot, so it is safe to cache.
 			if (isset($this->_classStore[$cacheId]) === false) {
 
-				$osObj				= null;
-				
 				//we need 4 things to determine the correct class of OS
 				$osArch				= null;
 				$osType				= null;
 				$osName				= null;
-				$osMajorVersion		= null;
+				$osVersion			= null;
 					
 				$osDetail			= php_uname();
-				if (preg_match("/^Linux\s/", $osDetail)) {
+				if (preg_match("/^Linux\s/i", $osDetail)) {
 					$osType			= 'linux';
-
 					if (preg_match("/(x86_64|i386|i686)/i", $osDetail, $rawArch) == 1) {
 						$rawArch	= strtolower($rawArch[1]);
 						if ($rawArch == "x86_64") {
@@ -40,63 +37,45 @@ class OperatingSystem extends Base
 						} elseif ($rawArch == "i386" || $rawArch == "i686") {
 							$osArch	= 32;
 						}
-						
-					} else {
-						throw new \Exception(__METHOD__ . ">> OS Arch Not handled");
 					}
 					
-					$rFile		= "/etc/os-release";
-					$rFile2		= "/etc/redhat-release";
-					$rFile3		= "/etc/centos-release";
-					$cmdString	= null;
-					if (file_exists($rFile) === true) {
-						$cmdString		= "cat " . $rFile;
-					} elseif (file_exists($rFile2) === true) {
-						$cmdString		= "cat " . $rFile2;
-					} elseif (file_exists($rFile3) === true) {
-						$cmdString		= "cat " . $rFile3;
-					} else {
-						throw new \Exception(__METHOD__ . ">> Missing Release file");
-					}
-					
-					$cReturn	= null;
-					if ($cmdString !== null) {
-						$cReturn		= $this->shellExec($cmdString);
-					}
+					//systemd standardized the release file back in 2012
+					//lets always try that file first
+					$reFiles	= array();
+					$reFiles[]	= "/etc/os-release";
+					$reFiles	= array_unique(array_merge($reFiles, glob('/etc/*-release')));
 
-					if ($cReturn !== null) {
-
-						if (preg_match("/NAME=\"(CentOS Linux|Debian GNU\/Linux|Ubuntu|Arch Linux)\"/i", $cReturn, $rawName) == 1) {
-							$osName				= strtolower($rawName[1]);
-						} elseif (preg_match("/CentOS/i", $cReturn, $rawName) == 1) {
-							$osName				= "centos linux";
-						} else {
-							throw new \Exception(__METHOD__ . ">> OS Name Not handled");
-						}
-						
-						if ($osName == 'arch linux') {
-							$cmdString		= 'cat /proc/version';
-							$cReturn		= $this->shellExec($cmdString);
+					foreach ($reFiles as $reFile) {
+						if (file_exists($reFile) === true) {
+							$strCmd		= "cat " . $reFile;
+							$cReturn	= $this->shellExec($strCmd);
 							
-							preg_match("/([0-9]{8})/", $cReturn, $rawMajorVersion);
-							
-							if (isset($rawMajorVersion[1]) === true) {
-								$osMajorVersion		= $rawMajorVersion[1];
+							if ($osName === null) {
+								if (preg_match("/(centos|debian|ubuntu|arch|red hat)/i", $cReturn, $rawName) == 1) {
+									$osName		= strtolower($rawName[1]);
+								}
+							}
+							if ($osVersion === null) {
+								if (preg_match("/VERSION_ID=\"([0-9]+)/", $cReturn, $rawVer) == 1) {
+									$osVersion		= $rawVer[1];
+								} elseif (preg_match("/release\s([0-9]+)/i", $cReturn, $rawVer) == 1) {
+									$osVersion		= $rawVer[1];
+								} elseif ($osName == 'arch') {
+									//Arch dists have their version in a different location
+									$archCmd	= "cat /proc/version";
+									$archReturn	= $this->shellExec($archCmd);
+									if (preg_match("/([0-9]{8})/", $archReturn, $rawVer) == 1) {
+										$osVersion		= $rawVer[1];
+									}
+								}
 							}
 							
-						} else {
-							
-							if (preg_match("/VERSION_ID=\"([0-9]+)/", $cReturn, $rawMajorVersion) == 1) {
-								$osMajorVersion		= $rawMajorVersion[1];
-							} elseif (preg_match("/([0-9]+)/", $cReturn, $rawMajorVersion) == 1) {
-								//solve permanentely for centos minor 7,8 "CentOS release 6.8 (Final)"
-								$osMajorVersion		= $rawMajorVersion[1];
-							} else {
-								throw new \Exception(__METHOD__ . ">> OS Version Not handled");
+							if ($osName !== null && $osVersion !== null) {
+								break;
 							}
 						}
 					}
-					
+
 				} elseif (preg_match("/^Windows\s/i", $osDetail)) {
 					
 					$osType			= 'windows';
@@ -106,8 +85,8 @@ class OperatingSystem extends Base
 					$cReturn		= $this->shellExec($cmdString);
 					preg_match("/Microsoft\s+Windows\s+(.+?)\|/i", $cReturn, $rawVersion);
 					if (isset($rawVersion[1])) {
-						$rawVer			= strtolower($rawVersion[1]);
-						$osMajorVersion	= $rawVer;
+						$rawVer		= strtolower($rawVersion[1]);
+						$osVersion	= $rawVer;
 					}
 					
 					$cmdString		= "wmic OS get OSArchitecture";
@@ -121,43 +100,49 @@ class OperatingSystem extends Base
 							$osArch	= 32;
 						}
 					}
-				} else {
-					throw new \Exception(__METHOD__ . ">> OS Type Not handled");
 				}
-
-				if (
-					$osType !== null
-					&& $osName !== null
-					&& $osMajorVersion !== null
-					&& $osArch !== null
-				) {
-					$osObj	= null;
-					if ($osType == 'linux') {
-						if ($osName == 'centos linux') {
-							$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\CentOSBase();
-						} elseif ($osName == 'red hat enterprise linux') {
-							$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\RHELBase();
-						} elseif ($osName == 'debian gnu/linux') {
-							$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\DebianBase();
-						} elseif ($osName == 'ubuntu') {
-							$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\UbuntuBase();
-						} elseif ($osName == 'arch linux') {
-							$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\ArchBase();
-						}
-					} elseif ($osType == 'windows') {
-						if ($osName == 'windows') {
-							$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Microsoft\Windows();
-						}
+				
+				if ($osType === null) {
+					throw new \Exception(__METHOD__ . ">> Could not determine OS Type");
+				} elseif ($osName === null) {
+					throw new \Exception(__METHOD__ . ">> Could not determine OS distribution");
+				} elseif ($osVersion === null) {
+					throw new \Exception(__METHOD__ . ">> Could not determine OS version");
+				} elseif ($osArch === null) {
+					throw new \Exception(__METHOD__ . ">> Could not determine OS Architecture");
+				}
+				
+				$osObj	= null;
+				if ($osType == 'linux') {
+					if ($osName == 'centos') {
+						$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\CentOSBase();
+					} elseif ($osName == 'red hat') {
+						$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\RHELBase();
+					} elseif ($osName == 'debian') {
+						$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\DebianBase();
+					} elseif ($osName == 'ubuntu') {
+						$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\UbuntuBase();
+					} elseif ($osName == 'arch') {
+						$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\ArchBase();
 					}
 					
-					if ($osObj !== null) {
-						$osObj->setMajorVersion($osMajorVersion);
-						$osObj->setArchitecture($osArch);
-						$this->_classStore[$cacheId]	= $osObj;
-						return $this->_classStore[$cacheId];
+				} elseif ($osType == 'windows') {
+					if ($osName == 'windows') {
+						$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Microsoft\Windows();
 					}
 				}
 				
+				if ($osObj !== null) {
+					
+					$osObj->setMajorVersion($osVersion);
+					$osObj->setArchitecture($osArch);
+					$this->_classStore[$cacheId]	= $osObj;
+					return $this->_classStore[$cacheId];
+					
+				} else {
+					throw new \Exception(__METHOD__ . ">> OS Type: " . $osType . " and name:" . $osName . ", not handled");
+				}
+
 			} else {
 				return $this->_classStore[$cacheId];
 			}
--- MTS/Common/Devices/Actions/Remote/Host/OperatingSystem.php
+++ MTS/Common/Devices/Actions/Remote/Host/OperatingSystem.php
@@ -16,168 +16,156 @@ class OperatingSystem extends Base
 		$requestType		= $this->_classStore['requestType'];
 		$shellObj			= $this->_classStore['shellObj']->getActiveShell();
 		$cacheId			= $requestType . "_" .$shellObj->getShellUUID();
-		
+	
 		if ($requestType == 'getOsObj') {
-			
+	
 			//the OS is constantly needed when other Actions want to determine what command syntax to use
 			//it also does not change without a reboot, so it is safe to cache.
 			if (isset($this->_classStore[$cacheId]) === false) {
-
-				$osObj				= null;
-				
+	
 				//we need 4 things to determine the correct class of OS
 				$osArch				= null;
 				$osType				= null;
 				$osName				= null;
-				$osMajorVersion		= null;
+				$osVersion			= null;
 				
 				//try to infer the OS type from the shell type.
 				if ($shellObj instanceof \MTS\Common\Devices\Shells\Bash) {
-	
-					$cmdString		= "cat /etc/os-release";
-					$cReturn		= $shellObj->exeCmd($cmdString);
-	
-					if (strlen($cReturn) == 0 || preg_match("/No such file/i", $cReturn) == 1) {
-						$cmdString		= "cat /etc/redhat-release";
-						$cReturn		= $shellObj->exeCmd($cmdString);
-						if (strlen($cReturn) == 0 || preg_match("/No such file/i", $cReturn) == 1) {
-							$cmdString		= "cat /etc/centos-release";
-							$cReturn		= $shellObj->exeCmd($cmdString);
-						}
-					}
+					//guessing Linux
 					
-					if (strlen($cReturn) > 0 && preg_match("/No such file/i", $cReturn) == 0) {
+					//systemd standardized the release file back in 2012
+					//lets always try that file first
+					$reFiles	= array();
+					$reFiles[]	= "/etc/os-release";
+					$reFiles[]	= "/etc/centos-release";
+					$reFiles[]	= "/etc/redhat-release";
+					$reFiles[]	= "/etc/system-release";
+					$reFiles[]	= "/etc/lsb-release";
+					
+					foreach ($reFiles as $reFile) {
 						
-						if (preg_match("/(CentOS|Linux|Ubuntu|Debian)/i", $cReturn)) {
-							$osType			= 'linux';
-		
-							$cmdString		= 'uname -a';
-							$uReturn		= $shellObj->exeCmd($cmdString);
-		
-							preg_match("/(x86_64|i386|i686)/i", $uReturn, $rawArch);
-							if (isset($rawArch[1])) {
-								$rawArch	= strtolower($rawArch[1]);
-								if ($rawArch == "x86_64") {
-									$osArch	= 64;
-								} elseif ($rawArch == "i386" || $rawArch == "i686") {
-									$osArch	= 32;
+						$strCmd		= "cat " . $reFile;
+						$cReturn	= $shellObj->exeCmd($strCmd);
+						
+						if (preg_match("/No such file/i", $cReturn) == 0) {
+						
+							if ($osName === null) {
+								if (preg_match("/(centos|debian|ubuntu|arch|red hat)/i", $cReturn, $rawName) == 1) {
+									$osName		= strtolower($rawName[1]);
+									$osType		= 'linux';
 								}
 							}
-													
-							if ($cReturn !== null) {
-								
-								if (preg_match("/NAME=\"(CentOS Linux|Debian GNU\/Linux|Ubuntu|Arch Linux)\"/i", $cReturn, $rawName) == 1) {
-									$osName				= strtolower($rawName[1]);
-								} elseif (preg_match("/CentOS/i", $cReturn, $rawName) == 1) {
-									$osName				= "centos linux";
-								} else {
-									throw new \Exception(__METHOD__ . ">> OS Name Not handled");
-								}
-						
-								if ($osName == 'arch linux') {
-									$cmdString		= 'cat /proc/version';
-									$c2Return		= $shellObj->exeCmd($cmdString);
-						
-									preg_match("/([0-9]{8})/", $c2Return, $rawMajorVersion);
-						
-									if (isset($rawMajorVersion[1]) === true) {
-										$osMajorVersion		= $rawMajorVersion[1];
-									}
-						
-								} else {
-									
-									if (preg_match("/VERSION_ID=\"([0-9]+)/", $cReturn, $rawMajorVersion) == 1) {
-										$osMajorVersion		= $rawMajorVersion[1];
-									} elseif (preg_match("/([0-9]+)/", $cReturn, $rawMajorVersion) == 1) {
-										//solve permanentely for centos minor 7,8 "CentOS release 6.8 (Final)"
-										$osMajorVersion		= $rawMajorVersion[1];
+							if ($osVersion === null) {
+								if (preg_match("/VERSION_ID=\"([0-9]+)/", $cReturn, $rawVer) == 1) {
+									$osVersion		= $rawVer[1];
+								} elseif (preg_match("/release\s([0-9]+)/i", $cReturn, $rawVer) == 1) {
+									$osVersion		= $rawVer[1];
+								} elseif ($osName == 'arch') {
+									//Arch dists have their version in a different location
+									$archCmd	= "cat /proc/version";
+									$archReturn	= $shellObj->exeCmd($archCmd);
+									if (preg_match("/([0-9]{8})/", $archReturn, $rawVer) == 1) {
+										$osVersion		= $rawVer[1];
 									}
 								}
+							}
+					
+							if ($osName !== null && $osVersion !== null) {
+								break;
 							}
 						}
 					}
 					
+					//architecture
+					$strCmd		= "uname -a";
+					$cReturn	= $shellObj->exeCmd($strCmd);
+					if (preg_match("/(x86_64|i386|i686)/i", $cReturn, $rawArch) == 1) {
+						$rawArch	= strtolower($rawArch[1]);
+						if ($rawArch == "x86_64") {
+							$osArch	= 64;
+						} elseif ($rawArch == "i386" || $rawArch == "i686") {
+							$osArch	= 32;
+						}
+					}
+						
 				} elseif ($shellObj instanceof \MTS\Common\Devices\Shells\RouterOS) {
-					
-					$osType				= 'mikrotik';
-					$osName				= 'routeros';
-					
+						
+					$osType			= 'mikrotik';
+					$osName			= 'routeros';
+						
 					$cmdString		= '/system resource print';
 					$reData			= $shellObj->exeCmd($cmdString);
-					
 					if (preg_match("/architecture-name:(.*)/", $reData, $rawAttr) == 1) {
-						$osArch				= trim($rawAttr[1]);
+						$osArch		= trim($rawAttr[1]);
 					}
 					if (preg_match("/version:\s([0-9]+)(\.[0-9]+)?/", $reData, $rawAttr) == 1) {
-						$osMajorVersion			= trim($rawAttr[1]);
+						$osVersion	= trim($rawAttr[1]);
 					}
-					
+						
 				} elseif ($shellObj instanceof \MTS\Common\Devices\Shells\PowerShell) {
-					
-					$osType				= 'windows';
-					$osName				= 'windows';
-					
-					$cmdString		= '(Get-WmiObject -class Win32_OperatingSystem).Name';
-					$reData			= $shellObj->exeCmd($cmdString);
-					
-					preg_match("/Microsoft\s+Windows\s+(.+?)\|/i", $reData, $rawVersion);
-					if (isset($rawVersion[1])) {
-						$rawVer			= strtolower($rawVersion[1]);
-						$osMajorVersion	= $rawVer;
+						
+					$osType			= 'windows';
+					$osName			= 'windows';
+						
+					$strCmd			= '(Get-WmiObject -class Win32_OperatingSystem).Name';
+					$reData			= $shellObj->exeCmd($strCmd);
+					if (preg_match("/Microsoft\s+Windows\s+(.+?)\|/i", $reData, $rawVersion) == 1) {
+						$osVersion	= strtolower($rawVersion[1]);
 					}
-					
-					$cmdString2		= '(Get-WmiObject -class Win32_OperatingSystem).OSArchitecture';
-					$reData2		= $shellObj->exeCmd($cmdString2);
-					
-					preg_match("/(64-bit|32-bit)/i", $reData2, $rawArch);
-					if (isset($rawArch[1])) {
-						$rawArch	= strtolower($rawArch[1]);
-						$osArch		= $rawArch;
+						
+					$strCmd			= '(Get-WmiObject -class Win32_OperatingSystem).OSArchitecture';
+					$reData			= $shellObj->exeCmd($strCmd);
+					if (preg_match("/(64-bit|32-bit)/i", $reData, $rawArch) == 1) {
+						$osArch		= strtolower($rawArch[1]);
 					}
 				}
-				
-				if (
-				$osType !== null
-				&& $osName !== null
-				&& $osMajorVersion !== null
-				&& $osArch !== null
-				) {
-					$osObj	= null;
-					if ($osType == 'linux') {
-						if ($osName == 'centos linux') {
-							$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\CentOSBase();
-						} elseif ($osName == 'red hat enterprise linux') {
-							$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\RHELBase();
-						} elseif ($osName == 'debian gnu/linux') {
-							$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\DebianBase();
-						} elseif ($osName == 'ubuntu') {
-							$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\UbuntuBase();
-						} elseif ($osName == 'arch linux') {
-							$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\ArchBase();
-						}
-					} elseif ($osType == 'mikrotik') {
-						if ($osName == 'routeros') {
-							$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Mikrotik\RouterOSBase();
-						}
-					} elseif ($osType == 'windows') {
-						if ($osName == 'windows') {
-							$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Microsoft\Windows();
-						}
+	
+				if ($osType === null) {
+					throw new \Exception(__METHOD__ . ">> Could not determine OS Type");
+				} elseif ($osName === null) {
+					throw new \Exception(__METHOD__ . ">> Could not determine OS distribution");
+				} elseif ($osVersion === null) {
+					throw new \Exception(__METHOD__ . ">> Could not determine OS version");
+				} elseif ($osArch === null) {
+					throw new \Exception(__METHOD__ . ">> Could not determine OS Architecture");
+				}
+	
+				$osObj	= null;
+				if ($osType == 'linux') {
+					if ($osName == 'centos') {
+						$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\CentOSBase();
+					} elseif ($osName == 'red hat') {
+						$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\RHELBase();
+					} elseif ($osName == 'debian') {
+						$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\DebianBase();
+					} elseif ($osName == 'ubuntu') {
+						$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\UbuntuBase();
+					} elseif ($osName == 'arch') {
+						$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Linux\ArchBase();
 					}
-				
-					if ($osObj !== null) {
-						$osObj->setMajorVersion($osMajorVersion);
-						$osObj->setArchitecture($osArch);
-						//add to cache
-						$this->_classStore[$cacheId]	= $osObj;
-						return $this->_classStore[$cacheId];
+						
+				} elseif ($osType == 'windows') {
+					if ($osName == 'windows') {
+						$osObj	= new \MTS\Common\Data\Computer\OperatingSystems\Microsoft\Windows();
 					}
 				}
-				
+	
+				if ($osObj !== null) {
+						
+					$osObj->setMajorVersion($osVersion);
+					$osObj->setArchitecture($osArch);
+					$this->_classStore[$cacheId]	= $osObj;
+					return $this->_classStore[$cacheId];
+						
+				} else {
+					throw new \Exception(__METHOD__ . ">> OS Type: " . $osType . " and name:" . $osName . ", not handled");
+				}
+	
 			} else {
 				return $this->_classStore[$cacheId];
 			}
 		}
+	
 		throw new \Exception(__METHOD__ . ">> Not Handled for Request Type: " . $requestType);
 	}
 }
